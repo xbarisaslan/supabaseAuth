3a6d0581ac49a64dcb108a1b74cb5c94d5a81747
Baris <baslan374@gmail.com>
2024-04-24 12:30:09 +0300

    initialize app

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen">
+    <h1 class="text-center font-bold text-[1.5rem] py-4">
+      Welcome to my Supabase Auth app!
+    </h1>
+  </div>
+</template>
+
+<script setup></script>
